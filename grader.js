a783841ec9e13b28b8908686d887382d962d8a2d
Ubuntu <ubuntu@ip-172-31-29-235.us-west-2.compute.internal>
2013-07-25 05:18:05 +0000

    Url version of grader.js

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://still-depths-5906.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,24 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL', URL_DEFAULT) 
         .parse(process.argv);
+    if(program.url !== URL_DEFAULT) {
+	        rest.get(program.url).on('complete', function(result) {
+		                if (result instanceof Error) {
+				                    console.log("Page not found!");
+				                    process.exit(1);
+				                } else {
+						                    console.log("Checking " + program.url);
+						                    checkUrl(result, program.checks);
+						                }
+      });
+}
+else{
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
+} }
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
